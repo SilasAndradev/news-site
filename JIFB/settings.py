6d2db81d38f057012356b921616b86d08e9c290c
Silas <silasandrade.code@gmail.com>
2025-04-24 19:57:20 -0300

    Alteração nas settings.py

--- JIFB/settings.py
+++ JIFB/settings.py
@@ -19,7 +19,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Acessando as variáveis de ambiente
-SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG")
 
