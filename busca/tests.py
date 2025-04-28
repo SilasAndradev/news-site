e97c670864ac75e5169456a9bdbbd3c1a4b673e4
Silas <silasandrade.code@gmail.com>
2025-04-28 20:23:05 -0300

    Tirei alguns arquivos desnecessários

--- busca/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 6dad46b28fb6462401944f0f9a31339fde9957db
